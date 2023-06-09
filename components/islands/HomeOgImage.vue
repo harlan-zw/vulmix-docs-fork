7d441d0e0aaded61d35009b5bb93bc040525a0c3
Harlan Wilton <harlan@harlanzw.com>
2023-06-09 10:28:49 +0800

    chore: test without images

--- components/islands/HomeOgImage.vue
+++ components/islands/HomeOgImage.vue
@@ -12,7 +12,6 @@
 <template>
   <div class="relative w-full h-full text-white bg-[#181818]">
     <div class="absolute top-0 right-0">
-      <img src="/img/pink-orb.png" />
     </div>
 
     <div class="absolute inset-0 flex flex-col items-center justify-center">
@@ -20,14 +19,12 @@
         class="flex z-10 absolute flex-col items-center justify-center gap-5"
       >
         <div class="mb-8">
-          <img src="/img/vulmix-logo.svg" class="h-56" />
         </div>
 
         <div class="text-white/70 text-4xl font-bold">vulmix.dev</div>
       </div>
 
       <div class="absolute bottom-0 left-0">
-        <img src="/img/green-orb.png" />
       </div>
     </div>
   </div>
