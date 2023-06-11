54b5fc913613adf21b3f45b80bff4563c7ca5a53
Harlan Wilton <harlan@harlanzw.com>
2023-06-11 23:32:38 +0800

    chore: maybe fix images

--- components/islands/HomeOgImage.vue
+++ components/islands/HomeOgImage.vue
@@ -12,7 +12,7 @@
 <template>
   <div class="relative w-full h-full text-white bg-[#181818]">
     <div class="absolute top-0 right-0">
-      <img src="/img/pink-orb.png" />
+      <img src="/img/pink-orb.png" width="1200" />
     </div>
 
     <div class="absolute inset-0 flex flex-col items-center justify-center">
@@ -20,14 +20,14 @@
         class="flex z-10 absolute flex-col items-center justify-center gap-5"
       >
         <div class="mb-8">
-          <img src="/img/vulmix-logo.svg" class="h-56" />
+          <img src="/img/vulmix-logo.svg" height="250" />
         </div>
 
         <div class="text-white/70 text-4xl font-bold">vulmix.dev</div>
       </div>
 
       <div class="absolute bottom-0 left-0">
-        <img src="/img/green-orb.png" />
+        <img src="/img/green-orb.png" width="1200" />
       </div>
     </div>
   </div>
